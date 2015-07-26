c08b9a82514052f9b7a788a981304dac0e2632aa
boleslaw wisniewski <boleslaw.wisniewski@gmail.com>
2015-07-25 20:42:06 -0400

    added default implementation (better than guice)

--- hk2/src/main/java/com/lab49/di/module/SamplesModule.java
+++ hk2/src/main/java/com/lab49/di/module/SamplesModule.java
@@ -1,12 +1,14 @@
 package com.lab49.di.module;
 
 import com.lab49.di.robotlegs.*;
+import org.glassfish.hk2.api.DescriptorVisibility;
 import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.hk2.api.ServiceLocatorFactory;
 import org.glassfish.hk2.utilities.BuilderHelper;
 import org.glassfish.hk2.utilities.ServiceLocatorUtilities;
 import org.glassfish.hk2.utilities.binding.AbstractBinder;
 
+import static org.glassfish.hk2.api.DescriptorVisibility.LOCAL;
 import static org.glassfish.hk2.utilities.BuilderHelper.createConstantDescriptor;
 import static org.glassfish.hk2.utilities.ServiceLocatorUtilities.addOneDescriptor;
 import static org.glassfish.hk2.utilities.ServiceLocatorUtilities.bind;
@@ -37,7 +39,14 @@ public class SamplesModule {
         protected void configure() {
             addActiveDescriptor(Foot.class);
             addActiveDescriptor(Body.class);
-            //can't default BigToe
+            //default BigToe implementation
+            addOneDescriptor(parentLocator, BuilderHelper
+                    .link(GnarlyBigToe.class)
+                    .to(BigToe.class)
+                    .in("org.glassfish.hk2.api.PerLookup")
+                    .visibility(LOCAL)
+                    .build()
+            );
 
             createChildLocator("Samples.Left", GnarlyBigToe.class, LeftLegFactory.class,
                     "com.lab49.di.module.Left");
@@ -52,17 +61,19 @@ public class SamplesModule {
             ServiceLocator childLocator = factory.create(locatorName, parentLocator);
             ServiceLocatorUtilities.addClasses(childLocator, Foot.class);
             ServiceLocatorUtilities.addClasses(childLocator, Body.class);
+            //add child locator so we can inject it
             addOneDescriptor(
                     parentLocator,
                     createConstantDescriptor(childLocator, locatorName, ServiceLocator.class)
             );
+            //bind child specific implementation of BigToe
             addOneDescriptor(childLocator, BuilderHelper
                     .link(implementation)
                     .to(BigToe.class)
                     .in("org.glassfish.hk2.api.PerLookup")
-                    .ofRank(1)
                     .build()
             );
+            //bind the annotated Leg Factory
             bind(BuilderHelper
                 .link(factoryImplementation)
                 .to(Leg.class)
